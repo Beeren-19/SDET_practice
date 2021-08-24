0f82af433d2b6df44c0253449a60358d1c02e8e6
Beeren-19 <gkbeerendrakumar01@gmail.com>
2021-08-24 16:34:13 +0530

    for bharath

---
+++ src/test/java/practice/Class6forbharath.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class Class6forbharath {
+ public static void main(String[] args) {
+	
+	 System.out.println("world");
+}
+}
---
+++ src/test/java/practice/Class7forbharath.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class Class7forbharath {
+ public static void main(String[] args) {
+	
+	 System.out.println("world");
+}
+}
