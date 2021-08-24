4e3e66499b99134c694c7f4bdcd8912a6ccdee8e
Beeren-19 <gkbeerendrakumar01@gmail.com>
2021-08-24 16:13:21 +0530

    for beeren

---
+++ src/test/java/practice/Class4forbeeren2.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Class4forbeeren2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("beeren1");
+		
+		
+	}
+}
---
+++ src/test/java/practice/Classf5orbeeren.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Classf5orbeeren {
+
+	public static void main(String[] args) {
+		
+		System.out.println("beeren2");
+		
+		
+	}
+}
